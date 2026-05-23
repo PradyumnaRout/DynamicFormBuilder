d27cc302251fc7f31c2b688c119f5af028fa542d
Pradyumna Rout <prdyumnarout1433@gmail.com>
2026-05-23 17:39:00 +0530

    Initial Commit

---
+++ DynamicFormBuilder/DynamicFormBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFormBuilderApp.swift
+//  DynamicFormBuilder
+//
+//  Created by Rahul Kiumar on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFormBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
